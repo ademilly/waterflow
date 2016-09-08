6c585e664577a3a83fc5de82417b1adb94730dfb
Aurélien Demilly <demilly.aurelien@gmail.com>
2016-09-08 18:05:14 +0200

    Add doc to ml.py

--- waterflow/ml.py
+++ waterflow/ml.py
@@ -14,16 +14,33 @@ class ML(dict):
         self.meta = {}
 
     def fit(self, X, y):
+        """Fit classifier to X, y
+
+        Keyword arguments:
+        X   ([entries x features] matrix) -- data
+        y   ([entries] vector) -- target
+        """
 
         self.clf.fit(X, y)
         return self
 
     def predict_proba(self, X):
+        """Build the probabilities vector of X
+
+        Keyword arguments:
+        X   ([entries x features] matrix) -- data
+        """
 
         self.meta['probabilities'] = self.clf.predict_proba(X)
         return self
 
     def log_loss(self, X, y):
+        """Evaluate log_loss metric from X and y
+
+        Keyword arguments:
+        X   ([entries x features] matrix) -- data
+        y   ([entries] vector) -- target
+        """
 
         if 'probabilities' not in self.meta.keys():
             self.predict_proba(X)
